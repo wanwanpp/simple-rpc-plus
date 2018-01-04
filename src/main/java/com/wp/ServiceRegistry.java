09007275090fc9990f36d71dd87a5414aa4ba304
王萍 <wang.03.25@qq.com>
2018-01-04 23:12:39 +0800

    注册中心ServiceRegistry

---
+++ src/main/java/com/wp/ServiceRegistry.java
@@ -0,0 +1,63 @@
+package com.wp;
+
+import org.apache.zookeeper.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+/**
+ * Created by 王萍
+ * on 2018/1/3 0031.
+ */
+public class ServiceRegistry {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceRegistry.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    private String registryAddress;
+
+    public ServiceRegistry(String registryAddress) {
+        this.registryAddress = registryAddress;
+    }
+
+    public void register(String data) {
+        if (data != null) {
+            ZooKeeper zk = connectServer();
+            if (zk != null) {
+                createNode(zk, data);
+            }
+        }
+    }
+
+    //连接zookeeper服务器
+    private ZooKeeper connectServer() {
+        ZooKeeper zk = null;
+        try {
+            zk = new ZooKeeper(registryAddress, Constant.ZK_SESSION_TIMEOUT, new Watcher() {
+                @Override
+                public void process(WatchedEvent event) {
+                    if (event.getState() == Event.KeeperState.SyncConnected) {
+                        latch.countDown();
+                    }
+                }
+            });
+            latch.await();
+        } catch (IOException | InterruptedException e) {
+            LOGGER.error("", e);
+        }
+        return zk;
+    }
+
+    //创建节点
+    private void createNode(ZooKeeper zk, String data) {
+        try {
+            byte[] bytes = data.getBytes();
+            String path = zk.create(Constant.ZK_DATA_PATH, bytes, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            LOGGER.debug("create zookeeper node ({} => {})", path, data);
+        } catch (KeeperException | InterruptedException e) {
+            LOGGER.error("", e);
+        }
+    }
+}
